ad86d82cdc2d236643e506f63c6444c586891c3d
MasterNikPro <nikzakharenkov@gmail.com>
2025-05-17 16:54:37 +0300

    fixed vuln bank can atack with fixed amount

--- tempvuln/EtherStore.sol
+++ tempvuln/EtherStore.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.26;
 
-contract VulnerableBank {
+contract EtherStore {
     mapping(address => uint256) public balances;
 
     function deposit() public payable {
@@ -10,15 +10,15 @@ contract VulnerableBank {
 
     function withdraw() public {
         uint256 bal = balances[msg.sender];
-        require(bal > 0, "Nothing to withdraw");
+        require(bal > 0);
 
-        // Уязвимость: внешняя call до обновления баланса
-        (bool sent, ) = msg.sender.call{value: bal}("");
+        (bool sent,) = msg.sender.call{value: bal}("");
         require(sent, "Failed to send Ether");
 
         balances[msg.sender] = 0;
     }
 
+    // Helper function to check the balance of this contract
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
